43f0aea422b0d1af80c3a4f5fc3da1e1a8e07899
muhammad farhan <144135382+ReiNA1169@users.noreply.github.com>
2023-09-26 13:57:29 +0700

    Update constants.js

--- latihan buat portofolio/src/data/constants.js
+++ latihan buat portofolio/src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/Hanzx69",
+    github: "https://github.com/ReiNA1169",
     resume:
       "https://api.whatsapp.com/send/?phone=089503826055&text&type=phone_number&app_absent=0",
     insta: "https://www.instagram.com/mhd.farhn__/",
